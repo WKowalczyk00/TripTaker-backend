16225db0589c3217e083f73ea33fad210b336474
Wojciech Kowalczyk <wojkowalczyk@student.agh.edu.pl>
2025-07-11 18:52:44 +0200

    minor changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'To są małe zmiany w pliku Wojtek Test'
+    return 'Hello World'
 
 
 if __name__ == '__main__':
